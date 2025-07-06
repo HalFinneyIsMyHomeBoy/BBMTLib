6d70484f739eda02878a79b5ab117a58750bc136
HalFinneyIsMyHomeBoy <primusbase@protonmail.com>
2025-07-06 16:24:23 -0500

    script fixes, working

--- scripts/main.go
+++ scripts/main.go
@@ -187,9 +187,9 @@ func main() {
 	if mode == "nostrKeygen" {
 		// prepare args
 
-		parties := "peer1,peer2"     // All participating parties
-		session := randomSeed(64)    // Generate random session ID
-		sessionKey := randomSeed(64) // Random session key
+		parties := "peer1,peer2,peer3" // All participating parties
+		session := randomSeed(64)      // Generate random session ID
+		sessionKey := randomSeed(64)   // Random session key
 		chainCode := randomSeed(64)
 		server := ""
 
