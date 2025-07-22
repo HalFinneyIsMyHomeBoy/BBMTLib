d0c65ec156f994073fdec51bb93ef56fff818c1b
HalFinneyIsMyHomeBoy <primusbase@protonmail.com>
2025-07-22 15:47:17 -0500

    All parties needed for keygen bug fix

--- tss/nostrcomm.go
+++ tss/nostrcomm.go
@@ -704,30 +704,46 @@ func initiateNostrHandshake(SessionID, chainCode, sessionKey, localParty, partyN
 	for retryCount <= maxRetries {
 		for _, item := range nostrSessionList {
 			if item.SessionID == SessionID {
-				participantCount := len(item.Participants)
-				aboveThreshold := participantCount >= 1+partyCount/2
-				if aboveThreshold {
-					Logf("Enough participants have approved, sending %s for session: %s", functionType, SessionID)
-					if item.Status == "pending" {
+
+				if functionType == "keygen" {
+					participantCount := len(item.Participants)
+
+					if participantCount == partyCount {
+						Logf("All participants have approved, sending %s for session: %s", functionType, SessionID)
 						sessionReady = true
 						startSessionMaster(SessionID, item.Participants, localParty, functionType)
-					} else {
-						return false, fmt.Errorf("session not ready")
 					}
-					return sessionReady, nil
 				} else {
-					if retryCount >= maxRetries {
-						if aboveThreshold {
-							Logf("We threshold of participants approved, sending %s for session: %s", functionType, SessionID)
+					participantCount := len(item.Participants)
+					participationRatio := float64(participantCount) / float64(partyCount)
+
+					if participationRatio >= 0.66 {
+						Logf("Enough participants have approved, sending %s for session: %s", functionType, SessionID)
+						if item.Status == "pending" {
 							sessionReady = true
 							startSessionMaster(SessionID, item.Participants, localParty, functionType)
 						} else {
-							Logf("Max retries reached, giving up on session: %s", SessionID)
-							return false, fmt.Errorf("max retries reached")
+							return false, fmt.Errorf("session not ready")
 						}
+						return sessionReady, nil
 					} else {
-						Logf("Waiting before retrying")
-						time.Sleep(KeysignApprovalTimeout)
+						if retryCount >= maxRetries {
+
+							participationRatio := float64(participantCount) / float64(partyCount)
+
+							if participationRatio >= 0.66 {
+
+								Logf("We have 2/3 of participants approved, sending %s for session: %s", functionType, SessionID)
+								sessionReady = true
+								startSessionMaster(SessionID, item.Participants, localParty, functionType)
+							} else {
+								Logf("Max retries reached, giving up on session: %s", SessionID)
+								return false, fmt.Errorf("max retries reached")
+							}
+						} else {
+							Logf("Waiting before retrying")
+							time.Sleep(KeysignApprovalTimeout)
+						}
 					}
 				}
 			}
